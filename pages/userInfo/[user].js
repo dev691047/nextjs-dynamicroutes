7f2161f5fdc59c7f24a37fb5afa4e2983afceb48
kumardev <dev691047@gmail.com>
2023-07-29 23:00:52 +0530

    firstcommit

---
+++ pages/userInfo/[user].js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
+const userInfo = () => {
+  const router = useRouter();
+  const Id = router.query.user;
+
+  const [searchQuery, setSearchQuery] = useState(null);
+  const [index, setIndex] = useState(null);
+  useEffect(() => {
+    console.log(`useEffect triggered`);
+    if (router.isReady) {
+      setSearchQuery(Id);
+
+      setIndex(details.filter((v) => v.id === Number(Id)));
+    }
+  }, [Id]);
+
+  return (
+    <div>
+      {index && (
+        <div>
+          {index[0].name},{index[0].role}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default userInfo;
